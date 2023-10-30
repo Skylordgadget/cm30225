a58bfbba20b538ea43e6b3f34f0581c6298e712b
Harry <harrykroby@aol.com>
2023-10-30 09:52:08 +0000

    updating frieden's code to latest version & adding 1s option

--- frieden.c
+++ frieden.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
 #include <pthread.h>
 #include <sys/time.h>
 #include <time.h>
 #include <math.h>
-#include <unistd.h>
+#include <stdlib.h>
 
 //#define _POSIX_C_SOURCE 199309L
 
-#define THREAD_LIMIT 4
-#define ROW 8
-#define COL 8
-#define PRECISION 1e-12
-#define DEBUG 0
-#define PRINT 1
+#ifndef CLOCK_MONOTONIC
+#define CLOCK_MONOTONIC 1
+#endif 
 
-struct timespec begin, end;
+// global
+int THREAD_LIMIT = 4;  
+int MAT_SIZE=100;
+double PRECISION=0.0000001;
+int DEBUG=0;
+int PRINT=0;
+
+struct timespec begin_time, end_time;
 double elapsed;
 
-//pthread_barrier_t my_barrier;
+pthread_barrier_t my_barrier;
 pthread_cond_t all_threads_finished;
-int count = 0;
 int finished_threads=0;
 
 typedef struct a{
@@ -29,43 +34,24 @@ typedef struct a{
     double** t_array;
     int start_row;
     int end_row;
-    int finished;
     pthread_mutex_t* lock;
     double difference;
-    int loop;
+    int mat_size;
 } threadArgs;
-
-//double** initArray(double** array_name, int row_size, int col_size){
-//    array_name=(double **)malloc(row_size*sizeof(double));
-//    if (array_name == NULL){
-//        exit(0);
-//    }
-//    int i;
-//    for (i=0; i<row_size; i++){
-//        array_name[i]=(double *)malloc(col_size*sizeof(double));
-//        if (array_name[i] == NULL){
-//            exit(0);
-//        } 
-//    }
-//    return(array_name);
-//}
 
 double** initArray(int row_size, int col_size) {
     double** array_name = (double **)malloc(row_size * sizeof(double*));
     if (array_name == NULL) {
         exit(0);
     }
-
     for (int i = 0; i < row_size; i++) {
         array_name[i] = (double *)malloc(col_size * sizeof(double));
         if (array_name[i] == NULL) {
             exit(0);
         }
     }
-
     return array_name;
 }
-
 
 void freeArray(double** array_name, int row_size){
     int i;
@@ -75,11 +61,11 @@ void freeArray(double** array_name, int row_size){
     free(array_name);
 }
 
-void printMatrix(double** array){
+void printMatrix(double** array, int size){
     printf("\n");
     int i,j;
-	for (i=0; i<ROW; i++){
-		for(j=0; j<COL; j++){
+	for (i=0; i<size; i++){
+		for(j=0; j<size; j++){
             printf("%.12f ",array[i][j]);
 		}
 		printf("\n");
@@ -88,171 +74,161 @@ void printMatrix(double** array){
 }
 
 void* average(void* args){
+    int precisionmet=0;
+    int thread_done=0;
     threadArgs* tArgs = (threadArgs*) args;
-
-    while (1) {
-        //printf("Start of the loop for thread %d\n",tArgs->thread_id);
+    // averaging values
+    while(1){
         tArgs->difference = 0.0;
-
         int i,j; 
-        for (i=tArgs->start_row; i<tArgs->end_row; i++){
-            for (j=0; j<COL; j++){
-                if (DEBUG) { printf("thread: %d, row: %d, col %d\n", tArgs->thread_id, i, j); }
-                if (i != 0 && i < ROW-1 && j != 0 && j < COL-1){
-                    tArgs->t_array[i][j]=(tArgs->array[i-1][j]+tArgs->array[i+1][j]+tArgs->array[i][j+1]+tArgs->array[i][j-1])/4;
+        if(!thread_done){
+            for (i=tArgs->start_row; i<tArgs->end_row+1; i++){
+                for (j=1; j<tArgs->mat_size+1; j++){
+                    if (DEBUG) { printf("thread: %d, row: %d, col %d\n", tArgs->thread_id, i, j); }
+                    tArgs->t_array[i][j]=\
+                                        (tArgs->array[i-1][j]+tArgs->array[i+1][j]
+                                        +tArgs->array[i][j+1]+tArgs->array[i][j-1])/4;
                 }
             }
         }
 
-        // Wait for all threads to be finished before reiterating
-        pthread_mutex_lock(tArgs->lock);
-        count++;
-        if (DEBUG) { printf("Count: %d\n",count); }
-        //printf("difference: %f. Thread %d\n", tArgs->difference, tArgs->thread_id);
-        if (count < THREAD_LIMIT - finished_threads) {
-            if (DEBUG) { printf("Thread %d is waiting for the first condition.\n",tArgs->thread_id); }
-            pthread_cond_wait(&all_threads_finished, tArgs->lock);
-        } else {
-            if (DEBUG) { printf("Thread %d is broadcasting to the other threads on the first condition.\n",tArgs->thread_id); }
-            pthread_cond_broadcast(&all_threads_finished);
-            count = 0;  // resetting count, so that the other threads can wait again.
-        }
-        pthread_mutex_unlock(tArgs->lock);
+        pthread_barrier_wait(&my_barrier);
 
-        for (i=tArgs->start_row; i<tArgs->end_row; i++){
-            for (j=0; j<COL; j++){
-                if (i != 0 && i < ROW-1 && j != 0 && j < COL-1){
-                    // t_array is the next iteration of the array. current - next = difference. 
-                    tArgs->difference += fabs(tArgs->array[i][j] - tArgs->t_array[i][j]);
+        // copying temp array values to shared array
+        if(!thread_done){
+            for (i=tArgs->start_row; i<tArgs->end_row+1; i++){
+                for (j=1; j<tArgs->mat_size+1; j++){
+                    tArgs->difference += fabs(tArgs->array[i][j] - tArgs->t_array[i][j]);   // t_array is the next iteration of the array. current - next = difference. 
                     tArgs->array[i][j]=tArgs->t_array[i][j]; // updating the array
                 }
             }
         }
 
-        // change difference???? 
 
-        // Wait for all threads to be finished before reiterating
-        //if (DEBUG) { printf("Thread %d has taken the lock.\n",tArgs->thread_id); }
-        pthread_mutex_lock(tArgs->lock);
-        count++;
-        if (DEBUG) { printf("Count: %d\n",count); }
-        //printf("difference: %f. Thread %d\n", tArgs->difference, tArgs->thread_id);
-        if (count < THREAD_LIMIT - finished_threads) {
-            if (DEBUG) { printf("Thread %d is waiting for the second condition.\n",tArgs->thread_id); }
-            pthread_cond_wait(&all_threads_finished, tArgs->lock);
-        } else {
-            count = 0;  // resetting count, so that the other threads can wait again.
-            // if the difference between all items (?) is less than the precision, we re-iterate again
+        // when done, set precisionmet = 1
+        if(!thread_done){
             if (tArgs->difference <= PRECISION) {
-                if (DEBUG) { printf("Thread %d is broadcasting and exiting.\n",tArgs->thread_id); }
+                thread_done=1;
+                pthread_mutex_lock(tArgs->lock);
                 finished_threads++;
-                pthread_cond_broadcast(&all_threads_finished);
-                // i think i've spent like 3 weeks trying to figure out why my code was hanging. i wasnt releasing the damn lock this whole time.
-                // or maybe something else in addition to this...
-                pthread_mutex_unlock(tArgs->lock);  
-                break;
-            } else {
-                if (DEBUG) { printf("Thread %d is broadcasting to the other threads on the second condition.\n",tArgs->thread_id); }
-                int rc = pthread_cond_broadcast(&all_threads_finished);
-                if (rc) { printf("error"); }
+                pthread_mutex_unlock(tArgs->lock);
             }
         }
-        pthread_mutex_unlock(tArgs->lock);
-        if (DEBUG) { tArgs->loop++; }
+        // Wait for all threads to be finished before reiterating
+        pthread_barrier_wait (&my_barrier);
+        if (finished_threads==THREAD_LIMIT) break;
     }
-
     if (DEBUG) { printf("Thread %d has exited the while loop\n",tArgs->thread_id); }
     if (DEBUG) { printf("Finished Threads: %d\n",finished_threads); }
-
-
     pthread_exit(NULL);
 }
 
 int main(int argc, char** argv){
 
-    clock_gettime(CLOCK_MONOTONIC, &begin);
+    // int option;
+    // while ((option = getopt(argc, argv, "n:s:p:do")) != -1) {
+    //     switch (option) {
+    //         case 'n':
+    //             THREAD_LIMIT = atoi(optarg); // Convert optarg to an integer
+    //             break;
+    //         case 's':
+    //             MAT_SIZE = atoi(optarg); // Convert optarg to an integer
+    //             break;
+    //         case 'p':
+    //             PRECISION = atof(optarg); // Convert optarg to a double
+    //             break;
+    //         case 'd':
+    //             DEBUG=1;
+    //             break;
+    //         case 'o':
+    //             PRINT=1;
+    //             break;
+    //         case '?':
+    //             puts("Unknown option or missing argument");
+    //     }
+    // }
+
+    int SIZE=MAT_SIZE-2;    // work on inner array   
+    // ensures number of threads < array size
+    //THREAD_LIMIT = 4;
+    THREAD_LIMIT = ( THREAD_LIMIT < SIZE ? THREAD_LIMIT : SIZE );
+
+    clock_gettime(CLOCK_MONOTONIC, &begin_time);
 
     // spawn threads to do work here
     pthread_mutex_t thread_lock = PTHREAD_MUTEX_INITIALIZER;
     pthread_cond_init(&all_threads_finished, NULL);
-    //pthread_barrier_init(&my_barrier, NULL, THREAD_LIMIT);
+    pthread_barrier_init(&my_barrier, NULL, THREAD_LIMIT);
 
     double** num_array = NULL;
     double** t_array = NULL;
-    num_array = initArray(ROW, COL);
-    t_array = initArray(ROW, COL);
+    num_array = initArray(MAT_SIZE, MAT_SIZE);
+    t_array = initArray(MAT_SIZE, MAT_SIZE);
 
     // fill boundary with random values
-    
     int i,j;
-    for (i=0; i<ROW; i++){
-        for (j=0; j<COL; j++){
-            if (i == 0 || i == ROW-1 || j == 0 || j == COL-1){ num_array[i][j] = rand()%ROW; t_array[i][j] = num_array[i][j]; }
+    for (i=0; i<MAT_SIZE; i++){
+        for (j=0; j<MAT_SIZE; j++){
+            if (i == 0 || i == MAT_SIZE-1 || j == 0 || j == MAT_SIZE-1){ num_array[i][j] = rand()%10; t_array[i][j] = num_array[i][j]; }
         }
     }
-    
-    /*
-    int i,j;
-    for (i=0; i<ROW; i++){
-        for (j=0; j<COL; j++){
-            if (i == 0 || j == 0) num_array[i][j] = 1.0; 
-            else num_array[i][j] = 0.0;
-            t_array[i][j] = num_array[i][j];
-        }
-    }
-    */
 
     // initialise the structs and threads 
     threadArgs tArgs[THREAD_LIMIT];
     pthread_t threads[THREAD_LIMIT];
 
-    if (PRINT) { printMatrix(num_array); }
+    if (PRINT) { printMatrix(num_array, MAT_SIZE); }
 
-    // creating the threads
-    int chunk = ceil((double) ROW / THREAD_LIMIT);
+    // number of threads which can do the larger work size
+    int max_work_thread_num = SIZE % THREAD_LIMIT;
+    double rows_per_thread = (double) SIZE / THREAD_LIMIT;
+    int max_rows_per_thread = ceil(rows_per_thread);
+    int min_rows_per_thread = floor(rows_per_thread);
+    
+    int start=0;
+    int end=0;
     for (i=0; i<THREAD_LIMIT; i++){
+        // if the number of threads divides evenly into the array size OR there are no more threads we can give the max work size
+        if (max_work_thread_num == 0){
+            // if this is the very first thread, start at 0. Else, start after the last thread
+            if (i == 0){ start = 1; } else { start = end+1; }
+            end = start+min_rows_per_thread-1; 
+        } else {
+            if (i == 0){ start = 1; } else { start = end+1; }
+            end = start+max_rows_per_thread-1; 
+            max_work_thread_num--;  // will eventually hit zero, so we will not go into this else statement again.
+        }
         tArgs[i].thread_id = i;
         tArgs[i].array=num_array;
         tArgs[i].t_array=t_array;
+        tArgs[i].start_row=start;
+        tArgs[i].end_row=end;
         tArgs[i].lock=&thread_lock;        
-        tArgs[i].start_row=i*chunk;
-        if (i == THREAD_LIMIT-1) {
-            tArgs[i].end_row = ROW;
-        }else{
-            tArgs[i].end_row=tArgs[i].start_row+chunk;
-        }
-        tArgs[i].finished=0;
-        tArgs[i].loop=0;
         tArgs[i].difference=0.0;
+        tArgs[i].mat_size=SIZE;
         int rc = pthread_create(&threads[i], NULL, average, (void*) &tArgs[i]);
         if (rc){
             printf("ERROR; return code from pthread_create() is %d\n", rc);
             exit(-1);
         }
-    }
+}
 
     // wait for all threads to finish their work
     for (i=0; i<THREAD_LIMIT; i++){
         pthread_join(threads[i], NULL);
     }
 
-    if (PRINT) { printMatrix(num_array); }
-
-    printf("Finished :)\n");
+    if (PRINT) { printMatrix(num_array, MAT_SIZE); }
 
     // all threads are finished, destroy stuff
     pthread_mutex_destroy(&thread_lock);
     pthread_cond_destroy(&all_threads_finished);
-
-    freeArray(num_array, ROW);
-    freeArray(t_array, ROW);
+    freeArray(num_array, SIZE);
+    freeArray(t_array, SIZE);
     
-
-    clock_gettime(CLOCK_MONOTONIC, &end);
-   
-
-    elapsed = end.tv_sec - begin.tv_sec;
-    elapsed += (end.tv_nsec - begin.tv_nsec) / 1000000000.0;
-    printf("Seconds %f\n", elapsed);
+    clock_gettime(CLOCK_MONOTONIC, &end_time);
+    elapsed = end_time.tv_sec - begin_time.tv_sec;
+    elapsed += (end_time.tv_nsec - begin_time.tv_nsec) / 1000000000.0;
+    printf("%f\n",elapsed);
     return 0;
 }
