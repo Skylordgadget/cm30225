eb07f96b3f4237c8930dd10511d8f473c7335335
Harry <harrykroby@aol.com>
2023-11-20 11:36:19 +0000

    making printf csv

--- hkr33_distributed_memory.c
+++ hkr33_distributed_memory.c
@@ -7,9 +7,7 @@ int main(int argc, char** argv){
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size_Of_Cluster);
     MPI_Comm_rank(MPI_COMM_WORLD, &process_Rank);
-
-    printf("Hello World from process %d of %d\n", process_Rank, size_Of_Cluster);
-
+    printf("Hello World from process, %d of, %d\n", process_Rank, size_Of_Cluster);
     MPI_Finalize();
     return 0;
 }
