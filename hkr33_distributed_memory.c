08cca62de5e7a9c63c16c9745da8fb95ab6046a2
Harry <harrykroby@aol.com>
2023-11-22 09:00:35 +0000

    simple MPI send, receive and broadcast

--- hkr33_distributed_memory.c
+++ hkr33_distributed_memory.c
@@ -7,7 +7,7 @@
 #include <stdbool.h>
 #include <time.h>
 
-#define DEBUG
+#undef DEBUG
 #define ROOT 0
 
 #ifdef _WIN32
@@ -15,6 +15,17 @@
 #elif __linux__
     #define SLASH "/"
 #endif
+
+struct thread_rows {
+    int start_row;
+    int end_row;
+};
+
+struct thread_data {
+    double precision;
+    int thread_lim;
+    int size;
+};
 
 void remove_spaces(char *str) {
     int count = 0;
@@ -26,28 +37,28 @@ void remove_spaces(char *str) {
     str[count] = '\0';
 }
 
-double** malloc_double_array(uint16_t size) {
+double** malloc_double_array(int size) {
     // malloc for the pointers to each row
     double** arr = (double**)malloc(size*sizeof(double*));
     // malloc for each element in each row  
-    for (uint16_t i=0; i<size; i++) 
+    for (int i=0; i<size; i++) 
         arr[i] = (double*)malloc(size*sizeof(double));
 
     return arr;
 }
 
-void free_double_array(double** arr, uint16_t size) {
+void free_double_array(double** arr, int size) {
     // free memory for each element in each row
-    for (uint16_t i=0; i<size; i++) free(arr[i]);
+    for (int i=0; i<size; i++) free(arr[i]);
     // free memory for the pointers to each row
     free(arr);
 }
 
 // populate 2D array based on selected mode
-double** debug_populate_array(double** arr, uint16_t size, char mode) {
+double** debug_populate_array(double** arr, int size, char mode) {
     srand(3);
-    for (uint16_t i=0; i<size; i++) {
-        for (uint16_t j=0; j<size; j++) {
+    for (int i=0; i<size; i++) {
+        for (int j=0; j<size; j++) {
             /* if at the top or left boundary, set value based on mode
             otherwise, set value 0 */
             
@@ -85,58 +96,99 @@ double** debug_populate_array(double** arr, uint16_t size, char mode) {
 }
 
 // print contents of 2D array
-void debug_display_array(double** arr, uint16_t size) {
-    for (uint16_t i=0; i<size; i++) { 
-        for (uint16_t j=0; j<size; j++) printf("%.12f ", arr[i][j]);
+void debug_display_array(double** arr, int size) {
+    for (int i=0; i<size; i++) { 
+        for (int j=0; j<size; j++) printf("%.12f ", arr[i][j]);
         printf("\n");
     }
     printf("\n");
 }
 
 // take 2D array and write contents into file using a file pointer
-void write_csv(double** arr, uint16_t size, FILE* fpt) {
-    for (uint16_t i=0; i<size; i++) { 
-        for (uint16_t j=0; j<size; j++) fprintf(fpt, "%.12f,", arr[i][j]);
+void write_csv(double** arr, int size, FILE* fpt) {
+    for (int i=0; i<size; i++) { 
+        for (int j=0; j<size; j++) fprintf(fpt, "%.12f,", arr[i][j]);
         fprintf(fpt, "\n");
     }
     fprintf(fpt, "\n");
 }
 
 // duplicate contents of 2D array arr_a into arr_b
-double** copy_array(double** arr_a, double** arr_b, uint16_t size) {
-    for (uint16_t i=0; i<size; i++)  
-        for (uint16_t j=0; j<size; j++) arr_b[i][j] = arr_a[i][j];
+double** copy_array(double** arr_a, double** arr_b, int size) {
+    for (int i=0; i<size; i++)  
+        for (int j=0; j<size; j++) arr_b[i][j] = arr_a[i][j];
     return arr_b;
+}
+
+MPI_Datatype create_trows_type(){
+    struct thread_rows trows;
+    MPI_Datatype trows_type;
+    MPI_Datatype type[2] = {MPI_INT, MPI_INT};
+    int blocklen[2] = {1, 1};
+    MPI_Aint disp[2];
+    MPI_Aint base;
+
+    MPI_Get_address(&trows, &base);
+    MPI_Get_address(&trows.start_row, &disp[0]);
+    MPI_Get_address(&trows.end_row, &disp[1]);
+    disp[0] = MPI_Aint_diff(disp[0], base);
+    disp[1] = MPI_Aint_diff(disp[1], base);
+
+    MPI_Type_create_struct(2, blocklen, disp, type, &trows_type);
+    MPI_Type_commit(&trows_type);
+    return trows_type;
+}
+
+MPI_Datatype create_tdata_type(){
+    struct thread_data tdata;
+    MPI_Datatype tdata_type;
+    MPI_Datatype type[3] = {MPI_DOUBLE, MPI_INT, MPI_INT};
+    int blocklen[3] = {1, 1, 1};
+    MPI_Aint disp[3];
+    MPI_Aint base;
+
+    MPI_Get_address(&tdata, &base);
+    MPI_Get_address(&tdata.precision, &disp[0]);
+    MPI_Get_address(&tdata.thread_lim, &disp[1]);
+    MPI_Get_address(&tdata.size, &disp[2]);
+    disp[0] = MPI_Aint_diff(disp[0], base);
+    disp[1] = MPI_Aint_diff(disp[1], base);
+    disp[2] = MPI_Aint_diff(disp[2], base);
+
+    MPI_Type_create_struct(3, blocklen, disp, type, &tdata_type);
+    MPI_Type_commit(&tdata_type);
+    return tdata_type;
 }
 
 int main(int argc, char** argv){
     int process_Rank, size_Of_Cluster;
+    struct thread_rows trows;
+    struct thread_data tdata;
 
     MPI_Init(&argc, &argv);
+
+    MPI_Datatype trows_type = create_trows_type();
+    MPI_Datatype tdata_type = create_tdata_type();
+
+
     MPI_Comm_size(MPI_COMM_WORLD, &size_Of_Cluster);
     MPI_Comm_rank(MPI_COMM_WORLD, &process_Rank);
     #ifdef DEBUG
         printf("%d\n", process_Rank);
     #endif
 
-    uint16_t    size;
-    uint16_t    thread_lim;
-    double      precision;
-    int         start_end_row[2];
 
     if (process_Rank == ROOT) {
         // FILE*       fpt;
         // command line arguments
-        int         num_threads_arg = -1; 
-        uint16_t    num_threads;
-        // uint16_t    thread_lim; 
+        int         num_threads = -1;
+        int         thread_lim;
 
-        int         size_arg = -1;
-        // uint16_t    size;
-        uint16_t    size_mutable;
+        int         size = -1;   
+        int         size_mutable;
 
-        double      precision_arg = -1.0;
-        // double      precision;
+        // double      precision_arg = -1.0;
+        double      precision = -1.0;
 
         // char*       file_path=NULL;
         // char        out_path[255];
@@ -166,13 +218,13 @@ int main(int argc, char** argv){
                 //     output = true;
                 //     break;
                 case 's': 
-                    size_arg = atoi(optarg);
+                    size = atoi(optarg);
                     break; 
                 case 't': 
-                    num_threads_arg = atoi(optarg); 
+                    num_threads = atoi(optarg); 
                     break; 
                 case 'p': 
-                    precision_arg = atof(optarg);
+                    precision = atof(optarg);
                     break; 
                 // case 'f':
                 //     remove_spaces(optarg);
@@ -198,28 +250,25 @@ int main(int argc, char** argv){
 
         /* num_threads, size and precision are required
         if -1 is not overwritten, the program will exit */
-        if (num_threads_arg == -1) {
+        if (num_threads == -1) {
             printf("ERROR -t required\n");     
             exit(1);   
         } else {
-            printf("using %d threads | ", num_threads_arg);
-            num_threads = (uint16_t)(num_threads_arg);
+            printf("using %d threads | ", num_threads);
         }
 
-        if (size_arg == -1) {
+        if (size == -1) {
             printf("ERROR -s required\n");
             exit(1);
         } else {
-            printf("size of %dx%d | ", size_arg, size_arg);
-            size = (uint16_t)(size_arg);
+            printf("size of %dx%d | ", size, size);
         }
 
-        if (precision_arg == -1) {
+        if (precision == -1) {
             printf("ERROR -p required\n");
             exit(1);
         } else {
-            printf("%f precision | ", precision_arg);
-            precision = precision_arg;
+            printf("%f precision | ", precision);
         }
 
         printf("mode %c\n", mode);
@@ -231,14 +280,18 @@ int main(int argc, char** argv){
         size_mutable = size - 2; // array borders are not mutable
         /* if there are more threads than rows in the array,
         set thread_lim = size_mutable, otherwise thread_lim = num_threads */  
-        thread_lim = (size_mutable < num_threads) ? size_mutable : num_threads;
+        thread_lim = (size_mutable < num_threads) ? size_mutable : num_threads-1;
         
+        tdata.precision = precision;
+        tdata.thread_lim = thread_lim;
+        tdata.size = size;
+
         /* if thread_lim is not divisible by the mutable array size,
         threads_l = number of threads that need to do more work */
-        uint16_t threads_l = size_mutable % thread_lim;
+        int threads_l = size_mutable % thread_lim;
 
-        uint16_t rows_per_thread_s = size_mutable / thread_lim;
-        uint16_t rows_per_thread_l = rows_per_thread_s + 1;
+        int rows_per_thread_s = size_mutable / thread_lim;
+        int rows_per_thread_l = rows_per_thread_s + 1;
         
         double** old_arr = malloc_double_array(size);
         double** new_arr = malloc_double_array(size);
@@ -248,24 +301,25 @@ int main(int argc, char** argv){
         
         if (print) debug_display_array(old_arr, size);
 
+        trows.start_row = 0;
+        trows.end_row = 0;
         
-
         //clock_gettime(CLOCK_REALTIME, &timer_start); // start timing code here
-        for (uint16_t i=0; i<thread_lim; i++) {
+        for (int i=0; i<thread_lim; i++) {
             // evenly distribute work among available threads
             if (threads_l > 0) {
-                start_end_row[0] = (i>0) ? start_end_row[1]+1 : 1;
-                start_end_row[1] = (uint16_t)(start_end_row[0] + rows_per_thread_l - 1);
+                trows.start_row = (i>0) ? trows.end_row+1 : 1;
+                trows.end_row = trows.start_row + rows_per_thread_l - 1;
                 threads_l--;
             } else {
-                start_end_row[0] = (i>0) ? start_end_row[1]+1 : 1;
-                start_end_row[1] = (uint16_t)(start_end_row[0] + rows_per_thread_s - 1);
+                trows.start_row = (i>0) ? trows.end_row+1 : 1;
+                trows.end_row = trows.start_row + rows_per_thread_s - 1;
             }
 
             if (verbose) {
-                printf("thread %d, start row: %d | end row %d\n", i, start_end_row[0], start_end_row[1]);
+                printf("thread %d, start row: %d | end row %d\n", i+1, trows.start_row, trows.end_row);
             }
-            MPI_Send(start_end_row, 2, MPI_INT, thread_lim+1, 99, MPI_COMM_WORLD);
+            MPI_Send(&trows, 1, trows_type, i+1, 99, MPI_COMM_WORLD);
         }
 
         // clock_gettime(CLOCK_REALTIME, &timer_end); // stop timing code
@@ -298,11 +352,14 @@ int main(int argc, char** argv){
 
         free_double_array(old_arr, size);
         free_double_array(new_arr, size);
+    //}
     } else {
         MPI_Status stat;
-        MPI_Recv(start_end_row, 2, MPI_INT, ROOT, 99, MPI_COMM_WORLD, &stat);
-        printf("I am thread: %d, my start row is %d, my end row is %d\n", process_Rank, start_end_row[0], start_end_row[1]);
+        MPI_Recv(&trows, 1, trows_type, ROOT, 99, MPI_COMM_WORLD, &stat);
+        printf("I am thread: %d, my start row is %d, my end row is %d\n", process_Rank, trows.start_row, trows.end_row);
     }
+    MPI_Bcast(&tdata, 1, tdata_type, ROOT, MPI_COMM_WORLD);
+    printf("I am thread: %d | p: %f | t: %d | s: %d\n", process_Rank, tdata.precision, tdata.thread_lim, tdata.size);
     MPI_Finalize();
     return 0;
 }
